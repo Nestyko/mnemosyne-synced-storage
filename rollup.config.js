6a291f02f6c4d672238e28c2e37a3f13ceb49865
Nestor Tobon <nestor.tobon@ibexlabs.com>
2022-06-18 14:31:31 -0400

    fallback to export all the functions to a single module

--- rollup.config.js
+++ rollup.config.js
@@ -2,43 +2,34 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from '@rollup/plugin-typescript'
+import image from '@rollup/plugin-image'
+import del from 'rollup-plugin-delete'
 import jsonPlugin from '@rollup/plugin-json'
 
-const plugins = [
-  peerDepsExternal(),
-  resolve(),
-  commonjs(),
-  jsonPlugin(),
-  typescript({
-    tsconfig: './tsconfig.json',
-  }),
-]
+import packageJson from './package.json'
 
-const indexFiles = [
-  './src/index.ts',
-  './src/react/index.ts',
-  './src/adapters/index.ts',
-]
-
-const config = indexFiles.map(inputFilePath => ({
-  input: inputFilePath,
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  input: './src/index.ts',
   output: [
     {
-      file: inputFilePath.replace('src', 'lib').replace('ts', 'js'),
+      file: packageJson.main,
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: inputFilePath
-        .replace('src', 'lib')
-        .replace('index', 'index.es')
-        .replace('ts', 'js'),
-      format: 'es',
+      file: packageJson.module,
+      format: 'esm',
       sourcemap: true,
     },
   ],
-  plugins,
-}))
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default config
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    jsonPlugin(),
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+  ],
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 export { MultiStorageAdapter, Adapter, AdapterOptions } from './storageAdapter'
+export { useSyncedStorage, MnemosyneContext, MnemosyneProvider } from './react'
+export {
+  chromeStorageAdapter,
+  urlParamsAdapter,
+  localStorageAdapter,
+} from './adapters'

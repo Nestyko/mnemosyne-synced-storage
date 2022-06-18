0995812e2280515c0e0dfe633a3cd555a965b568
Nestor Tobon <nestor.tobon@ibexlabs.com>
2022-06-18 14:49:42 -0400

    remove ReactNode dependency

--- src/react/components/Provider.tsx
+++ src/react/components/Provider.tsx
@@ -1,10 +1,10 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import { Adapter } from '../../storageAdapter'
 import { MnemosyneContext } from './Context'
 
 export const MnemosyneProvider: React.FC<{
   adapter: Adapter<any>
-  children: ReactNode
+  children: any
 }> = ({ adapter, children }) => {
   const Context = MnemosyneContext
   return <Context.Provider value={adapter}>{children}</Context.Provider>
